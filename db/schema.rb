8200bee733eaac4d97d14a0826982496bb06de97
mickael bitane <bitane.mickael@gmail.com>
2024-11-26 10:57:25 +0100

    modele et controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_160333) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_165111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "map_polyline"
+    t.integer "nb_of_players"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "email", default: "", null: false
