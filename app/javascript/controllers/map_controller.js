62fad58fbd473bb18d2e3405bd8633c7b355b2a6
Carlo <carlodhlln@gmail.com>
2024-11-28 14:09:11 +0100

    poly paris sur show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,55 +3,118 @@ import mapboxgl from "mapbox-gl" // Assurez-vous que mapbox-gl est bien importé
 
 export default class extends Controller {
   static values = {
-    apiKey: String,
-    polylines: Array
+    apiKey: String // Déclare l'apiKey pour utiliser dans le contrôleur
   }
 
   connect() {
     // Configure Mapbox
+    console.log("hello")
     mapboxgl.accessToken = this.apiKeyValue;
 
     // Initialiser la carte centrée sur Paris
-    this.map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
-      center: [2.3522, 48.8566],
+      center: [2.3522, 48.8566], // Paris
       zoom: 12
     });
 
-    this.addPolylines(); // Appelle la méthode pour ajouter les polylignes
-  }
+    // Lorsque la carte est chargée, ajoutez la source et le polygone
+      map.on('load', () => {
 
-  addPolylines() {
-    this.polylinesValue.forEach((polyline) => {
-      const coordinates = polyline.map(coord => [coord[0], coord[1]]); // Conversion si nécessaire
+        map.addSource('maine', {
+          'type': 'geojson',
+          'data': {
+              'type': 'Feature',
+              'geometry': {
+                  'type': 'Polygon',
+                  // These coordinates outline Maine.
+                  'coordinates': [
+                      [
+                        [2.25991, 48.84959],
+                        [2.26114, 48.85184],
+                        [2.28239, 48.87603],
+                        [2.30975, 48.89292],
+                        [2.34692, 48.89802],
+                        [2.38371, 48.89895],
+                        [2.40032, 48.88232],
+                        [2.41125, 48.8481],
+                        [2.39054, 48.8296],
+                        [2.36199, 48.81924],
+                        [2.32841, 48.82251],
+                        [2.29815, 48.82941],
+                        [2.25991, 48.84959]
+                      ]
+                  ]
+              }
+          }
+      });
 
-      // Création d'un objet GeoJSON directement au lieu d'utiliser mapboxgl.Polyline (non existant)
-      const route = {
-        type: 'Feature',
-        geometry: {
-          type: 'LineString',
-          coordinates: coordinates
-        }
-      };
-
-      // Ajout de la couche de polyligne à la carte
-      this.map.addLayer({
-        id: `route-${Math.random()}`,  // ID unique pour chaque couche
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: route
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#FF5733', // Couleur de la ligne
-          'line-width': 4
+      // Add a new layer to visualize the polygon.
+      map.addLayer({
+          'id': 'maine',
+          'type': 'fill',
+          'source': 'maine', // reference the data source
+          'layout': {},
+          'paint': {
+              'fill-color': '#0080ff', // blue color fill
+              'fill-opacity': 0.5
+          }
+      });
+      // Add a black outline around the polygon.
+      map.addLayer({
+        'id': 'outline',
+        'type': 'line',
+        'source': 'maine',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': 3
         }
       });
+    //   console.log('test')
+    //   // Ajout d'une source contenant des données GeoJSON pour le polygone
+    //   this.map.addSource('paris', {
+    //     'type': 'geojson',
+    //     'data': {
+    //       'type': 'Feature',
+    //       'geometry': {
+    //         'type': 'Polygon',
+    //         'coordinates': [
+    //           [
+    //             [48.84959, 2.25991],
+    //             [48.85184, 2.26114],
+    //             [48.87603, 2.28239],
+    //             [48.89292, 2.30975],
+    //             [48.89802, 2.34692],
+    //             [48.89895, 2.38371],
+    //             [48.88232, 2.40032],
+    //             [48.8481, 2.41125],
+    //             [48.8296, 2.39054],
+    //             [48.81924, 2.36199],
+    //             [48.82251, 2.32841],
+    //             [48.82941, 2.29815],
+    //             [48.84959, 2.25991]
+    //           ]
+    //         ]
+    //       }
+    //     }
+    //   });
+
+    //   // Ajout de la couche pour afficher le polygone
+    //   this.map.addLayer({
+    //     id: 'paris',  // ID unique pour la couche
+    //     type: 'fill',
+    //     source: 'paris',
+    //     layout: {},
+    //     paint: {
+    //       'fill-color': '#0080ff', // Couleur du polygone
+    //       'fill-opacity': 0.5,      // Transparence du polygone
+    //     }
+    //   });
+
+      // Ajouter des outils de zoom si nécessaire
+      map.addControl(new mapboxgl.NavigationControl());
     });
   }
 }
