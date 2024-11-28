528d4174f7e026a444c6550d15545676cfd932b8
Carlo <carlodhlln@gmail.com>
2024-11-28 16:31:44 +0100

    intermediaire

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,9 +8,15 @@ class Game < ApplicationRecord
   validates :status, presence: true, inclusion: { in: ["ongoing", "pending", "finish"] }
 
 
+  def surface
+    decoded_polyline = self.decoded_path
+    simplified_polyline = downsample_polyline(decoded_polyline)
+    polygon = polyline_to_polygon(simplified_polyline).area
+  end
 
+  private
 
-  def decoded_path_game
+  def decoded_path
     begin
     FastPolylines.decode(self.map_polyline)
     rescue StandardError => e
@@ -19,11 +25,43 @@ class Game < ApplicationRecord
     end
   end
 
-  def check_and_update_status!
-    if status == "pending" && players.size == nb_of_players
-      update!(status: "ongoing", start_date: Date.today, end_date: Date.today + duration.days)
-    end
+  def downsample_polyline(polyline)
+    step = case polyline.size
+           when 200..Float::INFINITY
+             10
+           when 100...200
+             5
+           when 50...100
+             3
+           when 20...50
+             2
+           else
+             1
+           end
+
+    return polyline if polyline.size <= 3 || step <= 1
+
+    first_point = polyline.first
+    last_point = polyline.last
+
+    downsampled = [first_point] + polyline[1...-1].each_slice(step).map(&:first) + [last_point]
+
+    # S'assurer que le dernier point est identique au premier pour fermer le polygone
+    downsampled[-1] = downsampled.first unless downsampled.last == downsampled.first
+
+    downsampled
   end
 
+  def polyline_to_polygon(polyline_decoded)
+    factory = RGeo::Geos.factory
+    points = polyline_decoded.map { |lat, lon| factory.point(lon, lat) }
+    polygon = factory.polygon(factory.linear_ring(points))
+    if polygon.valid?
+      polygon
+    else
+      polygon = polygon.make_valid
+    end
+
+  end
 
 end
