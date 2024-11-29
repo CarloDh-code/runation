613462338bbf4418ba858758b2523cd192c08180
mickael bitane <bitane.mickael@gmail.com>
2024-11-29 13:59:16 +0100

    fronthome

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_player!, only: [ :home ]
 
-  def home
-
+  class PagesController < ApplicationController
+    def home
+      if player_signed_in?
+        respond_to do |format|
+          format.html
+          format.js { render js: "setTimeout(function(){ window.location.href='#{games_path}'; }, 4000);" }
+        end
+      else
+        redirect_to new_player_session_path, alert: "Please log in to access the application."
+      end
+    end
   end
-  
+
+
 end
