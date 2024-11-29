d50a0374dbcecc9cae687b66e942ad6e98393189
mickael bitane <bitane.mickael@gmail.com>
2024-11-29 10:28:53 +0100

    chat/compoents/commentcontroller/navbar

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,8 +6,8 @@ class GamesController < ApplicationController
        unless @game.status == 'pending' || @game.players.include?(current_player)
         redirect_to games_path, alert: 'You can not access this game, sorry ! '
        end
-
-       # @polylines = @game.runs.includes(:polyline)
+        @comments = @game.comments.includes(:player) if @game.status == "ongoing"
+        # @polylines = @game.runs.includes(:polyline)
   end
 
   def index
