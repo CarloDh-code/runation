9429a5f99d45c16db0bda8b6c8c3ebfa33b87839
Taib Falilou Sall <staibfalilou@gmail.com>
2024-12-02 13:09:06 +0100

    Add dotenv-rails

--- app/javascript/controllers/user_profile.js
+++ app/javascript/controllers/user_profile.js
@@ -1,9 +1,42 @@
 import { Controller } from "@hotwired/stimulus";
+const cloudinary = require('cloudinary').v2;
 
-profileButton = document.getElementById("profileButton")
+import {vs as cloudinary} from "@cloudinary/url-gen";
+import { quality } from "@cloudinary/transformation-builder-sdk/actions/delivery";
 
-export default class extends Controller {
-  profileButton.addEventListener("click", (event) {
+cloudinary.config({
+  cloud_name: 'dnmhbfqjc',
+  secure: true,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+(async function() {
+  const results = await cloudinary.uploader.upload('/Users/admin/code/runation/app/assets/images/photo.png');
+  console.log(results);
+  const url = cloudinary.url(results.public_id, {
+    transformation: [
+      {
+        quality: 'auto',
+        fetch_format: 'auto'
+      },
+
+      {
+        width: 1200,
+        height: 1200,
+        crop: 'fill',
+        gravity: 'auto'
+      }
+    ]
+  })
+  console.log(url)
+})();
+
+
+
+  export default class extends Controller {
+    const profileButton = document.getElementById("profileButton");
+    profileButton.addEventListener ("click", (event) => {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
