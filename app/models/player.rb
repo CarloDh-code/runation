9556794257969a74dfc5f80edd6fa16bf83b5b34
Carlo <carlodhlln@gmail.com>
2024-12-03 13:02:14 +0100

    new seed

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,4 +10,23 @@ class Player < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def surface_covered_in_km2
+    # Calculer la surface totale couverte par le joueur en degrés carrés
+    total_surface_in_degrees = self.game_players.sum { |game_player| game_player.score }
+
+    # Conversion en km² en multipliant par 8547
+    total_surface_in_km2 = total_surface_in_degrees * 8547
+
+    # Arrondir à l'entier le plus proche (sans décimales)
+    total_surface_in_km2.to_i
+  end
+
+
+  def self.total_runs(player_id)
+    # Compter le nombre de runs associés à ce joueur à travers la table GamePlayerRuns
+    GamePlayerRun.joins(:game_player)
+                 .where(game_players: { player_id: player_id })
+                 .count
+  end
 end
