05733324f7baccb830c2708db3b2c48b773ad413
Carlo <carlodhlln@gmail.com>
2024-12-04 09:36:48 +0100

    solved notifs

--- db/migrate/20241203145712_create_notif_contents.rb
+++
@@ -1,9 +0,0 @@
-class CreateNotifContents < ActiveRecord::Migration[7.1]
-  def change
-    create_table :notif_contents do |t|
-      t.references :notification, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
--- test/models/notif_content_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class NotifContentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
