88a685db1d984d99aa78c9b7062eba2f933386ef
Antoine Rousselot <rousselotantoine7@gmail.com>
2024-12-05 15:28:49 +0100

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_player!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :home_page? 
+  helper_method :home_page?
 
   def home_page?
     controller_name == "pages" && action_name == "home"
@@ -15,4 +15,10 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :nickname])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+
 end
